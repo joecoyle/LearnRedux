eeb27e01e58acf7c80ab10b13328351b8cafac91
Joe Coyle <joecoyle@me.com>
2016-11-07 21:52:17 +0000

    Init commit

---
+++ app/app.jsx
@@ -0,0 +1,16 @@
+var React = require ('react');
+var ReactDOM = require ('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require ('react-router');
+
+
+// load foundation
+$(document).foundation();
+
+//app css
+require('style!css!sass!AppStyles');
+
+ReactDOM.render(
+  <p>React Boilerplate 3</p>,
+  document.getElementById('app')
+);
+require('./redux-example.jsx');
